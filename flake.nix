b3fc1db5502a7b5487314401cfa9266747ef47ed
Jeffrey C. Ollie <jeff@ocjtech.us>
2025-04-14 13:50:11 -0500

    update docker version

--- flake.nix
+++ flake.nix
@@ -24,7 +24,7 @@
           ];
         };
         lib = pkgs.lib;
-        docker-client = pkgs.docker_26.override {
+        docker-client = pkgs.docker_28.override {
           clientOnly = true;
         };
         git = pkgs.git.override {
